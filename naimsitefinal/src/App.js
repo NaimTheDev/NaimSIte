f6944ec5929c9cf36dccc913a819e37119c71f4b
Naim Abubaker <naimabubaker@Naims-MacBook-Air.local>
2020-08-04 18:37:14 -0400

    added naimsite folder to repo

---
+++ naimsitefinal/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import {Page1} from './Page1'
+
+
+
+function App() {
+  return (
+    <Page1/>
+  );
+}
+
+export default App;
---
+++ naimsitefinal/src/Page1.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+import './Page1.css';
+
+
+export class Page1 extends Component{
+
+        render(){
+            return(
+                <div className="Page1">
+                    <h1 className="Page1text">
+                    Hi, My Naim Abubaker, and I am a Software Engineer (Creator, Builder, Fun Guy) 
+                    </h1>
+                </div>
+            )
+        }
+
+
+
+}
